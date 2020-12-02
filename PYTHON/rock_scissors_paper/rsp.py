bd51de7bf5f03c07d724b5542f2f8e66232dc757
Back1ash <53644611+Back1ash@users.noreply.github.com>
2020-12-03 00:31:20 +0900

    12-02 rsp 잠깐

---
+++ PYTHON/rock_scissors_paper/rsp.py
@@ -0,0 +1,33 @@
+import cv2
+import random
+
+class rsp:
+    def make_cp(): #컴퓨터 가위바위보 선택. cp_pick이 변수명으로 더 나았을듯
+        rsp_list = ["가위", "바위", "보"]
+        return random.choice(rsp_list)
+    
+    def input_user(): #유저한테 가위바위보 중 하나 받기
+        while(1):   #무한 반복문으로 셋 중 하나만 입력하도록 만듦
+            a = input("가위, 바위, 보 중 하나만 내세요 : ")
+            if a != "가위" and a != "바위" and a != "보":
+                print("올바른 걸로 다시 입력하세요.")
+            else:
+                break
+        return a
+    def show_cp_pick(cp_pick):  #컴퓨터가 선택한 것 이미지로 화면에 띄우기
+        if cp_pick == "가위":
+            imgFile = "C:\\Users\\Haejun\\Desktop\\GitHub\\First_in_hufs\\PYTHON\\rock_scissors_paper\\rsp_image\\가위.png"
+        elif cp_pick == "바위":
+            imgFile = "C:\\Users\\Haejun\\Desktop\\GitHub\\First_in_hufs\\PYTHON\\rock_scissors_paper\\rsp_image\\바위.jpg"
+        elif cp_pick == "보":
+            imgFile = "C:\\Users\\Haejun\\Desktop\\GitHub\\First_in_hufs\\PYTHON\\rock_scissors_paper\\rsp_image\\보.png"
+        
+        img = cv2.imread(imgFile, cv2.IMREAD_COLOR)
+        cv2.imshow('가위_바위_보', img)
+
+        cv2.waitKey(500)
+        cv2.destroyAllWindows()
+    
+    make_cp()
+    b = input_user()
+    show_cp_pick(b)
--- PYTHON/rsp.py
+++
@@ -1,5 +0,0 @@
-import random
-
-class rsp:
-    def make_cp:
-        random.random
